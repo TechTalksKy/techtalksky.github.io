7798ebd2c9895cb4c027624065bfc21f09cbf864
Wes Eklund <wes.eklund@generationtux.com>
2016-09-09 10:46:45 -0400

    :pencil: style changes

--- resources/views/partials/siteInfo.blade.php
+++ resources/views/partials/siteInfo.blade.php
@@ -1,6 +1,6 @@
 <div class="site-info">
     <div class="primary-info">
-        <img width="396" height="177" src={{asset('img/tech_talks_website_logo.png')}} alt="">
+        <img width="396" height="177" src={{asset('img/techtalks-website-sidebar.png')}} alt="">
         <p>Welcome to Tech Talks KY!  A fun monthly event to talk about software and networking with other devs!</p>
     </div>
     <div class="secondary-info">
@@ -9,8 +9,7 @@
             @include('partials.facebook')
         </p>
         <p>
-            <a class="btn btn-primary" targt="_blank" href="{{ URL::to('https://wes37.typeform.com/to/JF00Dd') }}"><i class="fa fa-user-plus"></i>Join Our Slack Community</a>
+            <a class="btn btn-primary" target="_blank" href="{{ URL::to('https://wes37.typeform.com/to/JF00Dd') }}"><i class="fa fa-user-plus"></i>Join Our Slack Community</a>
         </p>
-        <br><br>
     </div>
 </div>
