00d3f8a0732a0d6ae4e884e8d9a41ce739eddcb4
Andrew huggins <andrewhuggins@gmail.com>
2016-09-08 21:59:42 -0400

    added Kre8 now

--- resources/views/posts/first-meetup.blade.php
+++ resources/views/posts/first-meetup.blade.php
@@ -16,7 +16,7 @@
 
         <p>Learn about the MVC architecture with examples from the Rails framework written in the Ruby programming language from Dave Hempy.</p>
 
-        <p>Dave has been programming for years, currently works at CirrusMio. And most recently participated in RedBull's Flugtag in Louisville.</p>
+        <p>Dave has been programming for years, currently works at CirrusMio. And most recently participated in RedBull's Flugtag in Louisville. Dave also helps run the <a href="http://www.kre8now.com/">Kre8now Makerspace</a>.</p>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/WDH8gso2yh4" frameborder="0" allowfullscreen></iframe>
     </div><!-- main-content/col -->
 </div>
