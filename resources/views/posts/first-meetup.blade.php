aecda12128565d9458d272cf65498cece6dc3f1e
Andrew huggins <andrewhuggins@gmail.com>
2016-09-08 22:00:30 -0400

    added word for better people make sense

--- resources/views/posts/first-meetup.blade.php
+++ resources/views/posts/first-meetup.blade.php
@@ -14,7 +14,7 @@
 
         <p class="subtitle">September 14, 2016 @ 6pm @ <a href="http://www.plantory.org/">Plantory</a></p>
 
-        <p>Learn about the MVC architecture with examples from the Rails framework written in the Ruby programming language from Dave Hempy.</p>
+        <p>Learn about the MVC architecture with examples from the Rails framework written in the Ruby programming language from speaker Dave Hempy.</p>
 
         <p>Dave has been programming for years, currently works at CirrusMio. And most recently participated in RedBull's Flugtag in Louisville. Dave also helps run the <a href="http://www.kre8now.com/">Kre8now Makerspace</a>.</p>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/WDH8gso2yh4" frameborder="0" allowfullscreen></iframe>
