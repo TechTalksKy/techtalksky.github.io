ca6c7e5d5554d82d9fed404a84309e5e989a52bc
Andrew huggins <andrewhuggins@gmail.com>
2016-10-04 00:26:12 -0400

    added presenter bar on daves talk page

--- resources/views/talk/dave-hempy.blade.php
+++ resources/views/talk/dave-hempy.blade.php
@@ -16,6 +16,24 @@
             <iframe width="560" height="315" src="https://www.youtube.com/embed/dO7D--52ois" frameborder="0" allowfullscreen></iframe>
         </div>
     </header>
+    <footer class="single">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-offset-1 col-xs-12 col-sm-2">
+                    <img src="http://www.redbullflugtag.com/usa-louisville/wp-content/uploads/sites/17/2016/06/David-H-532x398.jpeg" class="user-icon " alt="user-image">
+                </div>
+                <div class="col-xs-12 col-sm-6">
+                    <div class="category-list">
+                        <p>Presentation <span>September 14th 2016</span></p>
+                        <p>By Dave Hempy</p>
+
+                    </div>
+                </div><!-- end col -->
+            </div><!-- end row -->
+        </div>
+
+        
+    </footer>
 @stop
 
 @section('content')
